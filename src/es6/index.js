13d84b6ab5b298c1831f939cfd7c843f6e6e1696
Jhonkar Sufia <windyforest13@gmail.com>
2021-08-14 18:05:20 -0400

    Caracteristicas de ECMAScript 7

--- src/es6/index.js
+++ src/es6/index.js
@@ -186,3 +186,18 @@ console.log(calc.sum(2,3));
 import { hello } from './module';
 
 hello();
+
+//GENERATORS
+function* helloWorld() {
+    if (true) {
+        yield 'hello, ';
+    }
+    if (true) {
+        yield 'world!';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
---
+++ src/es7/index.js
@@ -0,0 +1,18 @@
+//INCLUDES
+let numbers = [1, 2, 3, 4, 6, 7, 8];
+
+if (numbers.includes(7)) {
+    console.log(`El numero 7 si esta en el array`);
+}
+else {
+    console.log(`No se encuentra`);
+}
+
+
+//OPERACIONES EN FORMA EXPONENCIAL
+let base = 4;
+let exponente = 3;
+
+let result = base ** exponente;
+
+console.log(result);
